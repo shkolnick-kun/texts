6bb0f84cfeea25d35cc3015d586608873df4d0e1
shkolnick-kun <shkolnick.kun@gmail.com>
2020-03-18 22:16:12 +0500

    Cleanup.

--- story5/sgp4ukf4.py
+++ story5/sgp4ukf4.py
@@ -19,9 +19,38 @@ _MINR = earth_radius_equatorial + 160e+3  # Perigee of the lowest orbit (stratos
 _MAXN = np.sqrt(earth_mu/_MINR**3) * 60. # Highest mean motion
 _MINN = np.sqrt(earth_mu/_MAXR**3) * 60. # Lowest mean motion
 
+_D2R   = np.pi/180.      # Degrees to radians
+_MPD   = 1440.           # Minutes in day
+_XD2RM = _MPD/(2.*np.pi) # rounds/ray. -> radians/minute
+
+#State sdt will be machine epsilones for values in TLE
+_AEPS = .5e-4 * _D2R
+#                  inclo,   nodeo,  ecco, argpo,     mo,     no_kozai
+_EPS_TLE = np.array([_AEPS,  _AEPS, .5e-7,  _AEPS, _AEPS, .5e-8*_XD2RM])
+
+
+_DIMX = 16 # The number of dtate dimmentions
+_DIMZ = 6  # The number of observation dimmentions
+#                  inclo,   nodeo,  ecco, argpo,     mo,     no_kozai
+_X_STD = np.array([1e-2]*_DIMX)
+_X_STD[:6] = _EPS_TLE
+
+#Default observation std
+_Z_STD = _EPS_TLE
+
 #State vector indexes
-_KEP_NUM = [0,2,5,6,7,8,9,10,11,12,13,14,15]
-_KEP_ANG = [1,3,4]   #Angle state dimensions
+#Angle state dimensions
+_KEP_ANG = [1,3,4]
+#Number like state dimensions
+_KEP_NUM = [i for i in range(_DIMX) if i not in _KEP_ANG]
+#==============================================================================
+#Get initial estimate of orbital elements
+def get_kepler_elements(r,v):
+    kep = elements_from_state_vector(np.array(r)*1000, np.array(v)*1000, earth_mu)
+    mo = mean_anomaly_from_true(kep.e, kep.f)
+    no_kozai = 60.0 * (earth_mu / (kep.a**3))**0.5
+    return np.array((kep.i, kep.raan, kep.e, kep.arg_pe, mo, no_kozai))
+
 #==============================================================================
 #Observation residual function
 def _res_kep(a,b):
@@ -39,7 +68,7 @@ def _res_kep(a,b):
     r[_KEP_ANG] = np.arctan2(sr, cr)
     
     return r
-
+#==============================================================================
 def _norm_kep(_x):
     x = _x.copy()
     for i in _KEP_ANG:
@@ -48,14 +77,7 @@ def _norm_kep(_x):
             x[i] = 2.*np.pi - x[i]
     return x
 #==============================================================================
-#Get initial estimate of orbital elements
-def get_kepler_elements(r,v):
-    kep = elements_from_state_vector(np.array(r)*1000, np.array(v)*1000, earth_mu)
-    mo = mean_anomaly_from_true(kep.e, kep.f)
-    no_kozai = 60.0 * (earth_mu / (kep.a**3))**0.5
-    return np.array((kep.i, kep.raan, kep.e, kep.arg_pe, mo, no_kozai))#.reshape((6,1))
-#==============================================================================
-def eval_tle(x, _epoch, _bstar):
+def _eval_sat(x, _epoch, _bstar):
     try:
         # Генерируем спутник
         ysat = PySatrec.new_sat(_epoch, 0, 0, _bstar, *list(x))
@@ -73,21 +95,12 @@ def eval_tle(x, _epoch, _bstar):
     # Возвращаем наблюдаемые значения элементов движения
     return get_kepler_elements(yr, yv), 0
 #==============================================================================
-_D2R   = np.pi/180.      # Degrees to radians
-_MPD   = 1440.           # Minutes in day
-_XD2RM = _MPD/(2.*np.pi) # rounds/ray. -> radians/minute
-
-#State sdt will be machine epsilones for values in TLE
-_AEPS = .5e-4 * _D2R
-#                  inclo,   nodeo,  ecco, argpo,     mo,     no_kozai
-_EPS_TLE = np.array([_AEPS,  _AEPS, .5e-7,  _AEPS, _AEPS, .5e-8*_XD2RM])
-
 def _eval_dif(dx, thr):
     return np.all(np.abs(dx) < thr * _EPS_TLE)
-    
-    
+#==============================================================================    
 # Подгонка модели 
-def fit_sgp4_bstar_fixed(__r, __v, __epoch, __bstar=0, g0 = 1., eps=.5, stop_thr=1e-0, max_iter=1000):
+def fit_sgp4_bstar_fixed(__r, __v, __epoch, __bstar=0, g0 = 1., eps=.5, 
+                         stop_thr=1e-0, max_iter=1000):
     #Наблюдаемые значения
     yo = get_kepler_elements(__r,__v)
 
@@ -99,51 +112,50 @@ def fit_sgp4_bstar_fixed(__r, __v, __epoch, __bstar=0, g0 = 1., eps=.5, stop_thr
 
     # Информация о знаках невязок
     sez = yo >= x
-    
+
     exc = 0
 
     for j in range(max_iter):
-        # Невязки
-        
-        y, exc = eval_tle(x, __epoch, __bstar)
+        y, exc = _eval_sat(x, __epoch, __bstar)
         if (exc):
             break
-
+        
+        # Невязки
         e = _res_kep(yo, y)
-       
+
         #Знаки невязок
         se = e > 0.
         changed = np.logical_xor(se, sez)
         sez = se
-    
+
         # Если знак не менялся - увеличиваем коэффициент, 
         # если менялся - уменьшаем
         gains *= (1. + eps)*np.logical_not(changed) + (1. - eps)*changed
         gains = np.clip(gains, 0., 1.)
-    
+
         dx = gains * e
         # Ограничения:
         # inclo[0, pi] ecco [0;.9999999], no_kozai [0; _MAXN]
         for i,ul in ((0, np.pi),(2, .9999999),(5, _MAXN)):
-            
+
             if dx[i] == 0:
                 continue
-            
+
             k = 1
-            
+
             if x[i] + dx[i] >= ul:
                 k = dx[i]/(ul - x[i])/(1. - eps)
-                
+
             if x[i] + dx[i] <= 0:
                 k = abs(dx[i]/x[i])/(1. - eps)
-            
+
             gains[i] /= k
             dx[i]    /= k
-    
+
         x += dx
-        
+
         x = _norm_kep(x)
-                
+
         if _eval_dif(dx, stop_thr):
             break
 
@@ -152,41 +164,31 @@ def fit_sgp4_bstar_fixed(__r, __v, __epoch, __bstar=0, g0 = 1., eps=.5, stop_thr
 
     return x, j, _eval_dif(dx, stop_thr), np.abs(dx)/_EPS_TLE, exc
 #==============================================================================
-def get_initial_estimate(r,v):
-    x = np.zeros((16,),dtype=np.float)
+def _get_initial_estimate(r,v):
+    x = np.zeros((_DIMX,),dtype=np.float)
     x[:6] = get_kepler_elements(r,v).reshape((6,))
     return x
-
-#Observation residual function
-def _res_pe(a,b):
-    s = b #Other hipoteses didn't work
-    rn = np.linalg.norm(s[:3])
-    vn = np.linalg.norm(s[3:])
-    d = a - b
-    d[:3] /= rn
-    d[3:] /= vn
-    return d
-
+#==============================================================================
 #State residual function
 def _res_sgp4(a,b):
     r = a - b
-    
+
     sa = np.sin(a[_KEP_ANG])
     ca = np.cos(a[_KEP_ANG])
-    
+
     sb = np.sin(b[_KEP_ANG])
     cb = np.cos(b[_KEP_ANG])
-    
+
     sr = sa * cb - ca * sb
     cr = cb * ca + sb * sa
-    
-    r[_KEP_ANG] = np.arctan2(sr, cr)
-    
-    return r
 
+    r[_KEP_ANG] = np.arctan2(sr, cr)
+
+    return r
+#==============================================================================
 #State mean function
 def _mean_sgp4(sigmas, Wm):
-    x = np.zeros(16)
+    x = np.zeros(_DIMX)
     #Sin and Cos of angles
     si = np.zeros(3)
     co = np.zeros(3)
@@ -204,14 +206,14 @@ def _mean_sgp4(sigmas, Wm):
     x[_KEP_ANG] = ang
 
     return x
-
+#==============================================================================
 # State transition function
-def sgp4_fx(x, dt, **fx_args):   
+def _sgp4_fx(x, dt, **fx_args):   
     return x
-
+#==============================================================================
 # Observation function
 # hx args: epoch, current
-def sgp4_hx(x, **hx_args):
+def _sgp4_hx(x, **hx_args):
     t1 = hx_args['current']
     t2 = t1*t1
     t3 = t2*t1
@@ -222,25 +224,9 @@ def sgp4_hx(x, **hx_args):
     ecco  = x[2] -  x[9] * t1
     argpo = x[3] + x[10] * t1
     mo    = x[4] + x[11] * t1
-    a     = x[5] - x[12] * t1 - x[13] * t2 - x[14] * t3 - x[15] * t4
-    return np.array((inclo, nodeo, ecco, argpo, mo, a))
-
-
-_D2R   = np.pi/180.      # Degrees to radians
-_MPD   = 1440.           # Minutes in day
-_XD2RM = _MPD/(2.*np.pi) # rounds/ray. -> radians/minute
-
-#State sdt will be machine epsilones for values in TLE
-_AEPS = .5e-4 * _D2R
-#                  inclo,   nodeo,  ecco, argpo,     mo,     no_kozai
-_X_STD = np.array([_AEPS,  _AEPS, .5e-7,  _AEPS, _AEPS, .5e-8*_XD2RM]+[1e-2]*10)
-
-#Default observation std
-_Z_STD = _X_STD[:6]
-
-_DIMX = 16 # The number of dtate dimmentions
-_DIMZ = 6  # The number of observation dimmentions
-
+    n     = x[5] - x[12] * t1 - x[13] * t2 - x[14] * t3 - x[15] * t4
+    return np.array((inclo, nodeo, ecco, argpo, mo, n))
+#==============================================================================
 class KalmanSGP4Estimator(object):
     def __init__(self, r, v, t, cb=None, m=.1, Filter=None, sp=None, R=None):
 
@@ -253,14 +239,14 @@ class KalmanSGP4Estimator(object):
             z,j,c,d,exc = fit_sgp4_bstar_fixed(ri, v[i], t[i], max_iter=50)
 
             if c:
-                zz.append(z.reshape((6,1)))
+                zz.append(z.reshape((_DIMZ,1)))
                 tt.append(t[i] - t[0])
 
             if cb:
                 cb(i)
                 
         zz = np.concatenate(zz, axis=1).T
-        zz[:, 5] = np.power(earth_mu*np.power((60 / zz[:,5]), 2.), (1. / 6.))
+        #zz[:, 5] = np.power(earth_mu*np.power((60 / zz[:,5]), 2.), (1. / 6.))
         self.z = zz
         self.t = tt
         self.epoch = t[0]
@@ -271,7 +257,7 @@ class KalmanSGP4Estimator(object):
         if Filter is None:
             Filter = UnscentedKalmanFilter
 
-        kf = Filter(dim_x=_DIMX, dim_z=_DIMZ, dt=dt, fx=sgp4_fx, hx=sgp4_hx, 
+        kf = Filter(dim_x=_DIMX, dim_z=_DIMZ, dt=dt, fx=_sgp4_fx, hx=_sgp4_hx, 
                     points=sp, residual_x=_res_sgp4, x_mean_fn=_mean_sgp4, 
                     residual_z=_res_kep)
 
@@ -311,8 +297,7 @@ class KalmanSGP4Estimator(object):
         
         return PySatrec.new_sat(self.epoch, 0, 0, bstar, *list(self.kf.x[:6]))
 
-
-
+#==============================================================================
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
     from progressbar import ProgressBar as bar
@@ -354,7 +339,7 @@ if __name__ == '__main__':
     x = []
     def kf_cb(estimator, i, j):
         pb.update(j)
-        y.append(np.linalg.norm(estimator.kf.y))
+        y.append(estimator.kf.y.reshape((_DIMZ,1)))
         x.append(estimator.kf.x[:6])
     
     pb = bar().start(len(est.z))
@@ -363,7 +348,8 @@ if __name__ == '__main__':
     #est.run_one_epoch(shuffle=True, cb=kf_cb)
     pb.finish()
     
-    plt.plot(y)
+    y = np.concatenate(y, axis=1).T
+    plt.plot(y[10:])
     print(xs.inclo    - est.kf.x[0])
     print(xs.nodeo    - est.kf.x[1])
     print(xs.ecco     - est.kf.x[2])
